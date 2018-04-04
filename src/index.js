a6c1227094cb9d5988595cc45426ed8585513b1a
Matthew Butler <matthewtbutler@gmail.com>
2018-04-04 12:23:05 -0500

    accept csv file and convert to geojson

--- src/index.js
+++ src/index.js
@@ -2,5 +2,37 @@ const _ = require('lodash')
 const csv2geojson = require('csv2geojson')
 const leaflet = require('leaflet')
 
-console.log('works')
+let button = document.querySelector("#csv-file + button")
+let input = document.getElementById("csv-file")
+let display = document.getElementById("DisplayText")
+let text = null
+input.addEventListener("change", addDoc)
+button.addEventListener("click", handleText)
+
+function addDoc(event) {
+  let file = this.files[0]
+  let reader = new FileReader()
+
+  reader.onload = function(e) {
+    text = reader.result
+    button.removeAttribute("disabled")
+  }
+
+  reader.onerror = function(err) {
+    console.log(err, err.loaded, err.loaded === 0, file)
+    button.removeAttribute("disabled")
+  }
+
+  reader.readAsText(event.target.files[0])
+}
+
+function handleText() {
+  csv2geojson.csv2geojson(text, function(err, data) {
+    display.textContent = JSON.stringify(data, null, 4)
+  })
+  
+  button.setAttribute("disabled", "disabled")
+  // set `text` to `null` if not needed or referenced again
+  text = null
+}
 
